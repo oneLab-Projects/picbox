2fdb7dce136bccf703de382c4bc33a2727f785a1
Semyon Butenko <laim0n@yandex.ru>
2019-10-31 19:56:17 +0300

    Update NotificationsTab

--- lib/tabs/notifications_tab.dart
+++ lib/tabs/notifications_tab.dart
@@ -6,7 +6,19 @@ class NotificationsTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return TabBody(
       title: 'Notifications',
-      child: Center(child: Text("Notifications")),
+      child: Column(
+        children: <Widget>[
+          for (var i = 0; i < 13; i++)
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+              height: 85,
+              margin: EdgeInsets.only(bottom: 15),
+            ),
+        ],
+      ),
     );
   }
 }
