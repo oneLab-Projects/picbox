72ec2faf571015d53fb4c467ebe25be79bd037e9
Semyon Butenko <laim0n@yandex.ru>
2019-11-16 22:23:07 +0300

    Update theme

--- lib/src/blocs/theme/day.dart
+++ lib/src/blocs/theme/day.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 ThemeData themeDay() => ThemeData(
       brightness: Brightness.light,
-      highlightColor: Color(0xFFFFFFFF),
+      highlightColor: Colors.grey[200],
       scaffoldBackgroundColor: Color(0xFFF8F8F8),
     );
--- lib/src/blocs/theme/night.dart
+++ lib/src/blocs/theme/night.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:picbox/src/common/design/style.dart';
 
 ThemeData themeNight() => ThemeData(
       brightness: Brightness.dark,
+      cardColor: Color(0xFFC7C7C7),
       highlightColor: Color(0xFF252525),
       scaffoldBackgroundColor: Color(0xFF1E1E1E),
+      textTheme: Style.textTheme,
     );
