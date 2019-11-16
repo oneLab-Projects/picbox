ea73a431093bbe1b04ed1baa2db6579390c89064
Semyon Butenko <laim0n@yandex.ru>
2019-11-16 22:23:26 +0300

    Update search page

--- lib/src/pages/tabs/search.dart
+++ lib/src/pages/tabs/search.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:picbox/src/common/bodies/tab.dart';
 
@@ -5,7 +7,60 @@ class SearchTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TabBody(
-      child: Center(child: Text("Search")),
+      child: Column(
+        children: <Widget>[
+          _buildSearchBar(context),
+          _buildTagBar(context),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).highlightColor,
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            height: 185,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearchBar(context) {
+    return TextField(
+      autocorrect: false,
+      decoration: InputDecoration(
+        hintText: 'Search',
+        contentPadding: EdgeInsets.symmetric(vertical: 14, horizontal: 18),
+        filled: true,
+        hintStyle: TextStyle(fontSize: 17, height: 1.33),
+        fillColor: Theme.of(context).highlightColor,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(50)),
+          borderSide: BorderSide(style: BorderStyle.none),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(50)),
+          borderSide: BorderSide(style: BorderStyle.none),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTagBar(context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 15),
+      height: 40,
+      child: Row(
+        children: <Widget>[
+          for (var i = 0; i < 3; i++)
+            Container(
+              width: 80 + Random().nextInt(30).toDouble(),
+              margin: EdgeInsets.only(right: 10),
+              decoration: BoxDecoration(
+                color: Theme.of(context).highlightColor,
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
