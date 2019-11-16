936cbc1cb742d33a206bfd00c6c148f3a9dad1e4
Semyon Butenko <laim0n@yandex.ru>
2019-11-16 03:01:45 +0300

    Removed tab View

--- lib/src/pages/tabs.dart
+++ lib/src/pages/tabs.dart
@@ -3,4 +3,3 @@ export 'package:picbox/src/pages/tabs/home.dart';
 export 'package:picbox/src/pages/tabs/notifications.dart';
 export 'package:picbox/src/pages/tabs/profile.dart';
 export 'package:picbox/src/pages/tabs/search.dart';
-export 'package:picbox/src/pages/tabs/view.dart';
