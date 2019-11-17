f0b98e85a6256e0c6617f41dce8355e11c2823f8
Semyon Butenko <laim0n@yandex.ru>
2019-11-17 19:05:22 +0300

    Update page search

--- lib/src/pages/tabs/search.dart
+++ lib/src/pages/tabs/search.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:picbox/src/common/bodies/tab.dart';
 
@@ -10,13 +8,13 @@ class SearchTab extends StatelessWidget {
       child: Column(
         children: <Widget>[
           _buildSearchBar(context),
-          _buildTagBar(context),
           Container(
             decoration: BoxDecoration(
               color: Theme.of(context).disabledColor,
               borderRadius: BorderRadius.all(Radius.circular(15)),
             ),
             height: 185,
+            margin: EdgeInsets.only(top: 10),
           ),
         ],
       ),
@@ -40,27 +38,6 @@ class SearchTab extends StatelessWidget {
           borderRadius: BorderRadius.all(Radius.circular(50)),
           borderSide: BorderSide(style: BorderStyle.none),
         ),
-      ),
-    );
-  }
-
-  Widget _buildTagBar(context) {
-    final random = Random();
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 15),
-      height: 40,
-      child: Row(
-        children: <Widget>[
-          for (var i = 0; i < 3; i++)
-            Container(
-              width: 80 + random.nextInt(30).toDouble(),
-              margin: EdgeInsets.only(right: 10),
-              decoration: BoxDecoration(
-                color: Theme.of(context).disabledColor,
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-              ),
-            ),
-        ],
       ),
     );
   }
