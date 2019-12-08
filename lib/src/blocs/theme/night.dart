8b9005ad2528b0070c2bde41b1b54a6bf884a2e8
Semyon Butenko <laim0n@yandex.ru>
2019-12-08 03:57:36 +0300

    Fix design

--- lib/src/blocs/theme/night.dart
+++ lib/src/blocs/theme/night.dart
@@ -4,7 +4,7 @@ import 'package:picbox/src/common/design/style.dart';
 final ThemeData themeNight = ThemeData(
   brightness: Brightness.dark,
   textTheme: Style.textTheme,
-  primaryColor: Colors.grey[100],
+  primaryColor: Colors.grey[200],
   scaffoldBackgroundColor: Color(0xFF1E1E1E),
   cursorColor: Colors.grey[500],
   disabledColor: Color(0xFF252525),
