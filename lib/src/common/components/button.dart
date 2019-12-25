ce6dc73a6e80b3e5dac7e90f2488c8c9fc9cd821
Semyon Butenko <laim0n@yandex.ru>
2019-12-26 01:12:14 +0300

    Update Button

--- lib/src/common/components/button.dart
+++ lib/src/common/components/button.dart
@@ -1,11 +1,19 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
-  final String text;
-  final IconData iconData;
-  final Function onTap;
+  Button(
+      {this.text,
+      this.iconData,
+      this.onTap,
+      this.onLongPress,
+      this.loading = false});
 
-  Button({this.text, this.iconData, this.onTap});
+  final IconData iconData;
+  final bool loading;
+  final Function onLongPress;
+  final Function onTap;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
@@ -14,36 +22,55 @@ class Button extends StatelessWidget {
       color: Theme.of(context).primaryColor,
       clipBehavior: Clip.antiAliasWithSaveLayer,
       child: InkWell(
-        onTap: onTap,
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              if (text != null)
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 12),
-                  child: Text(
-                    text,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.button.copyWith(
-                          color: onTap == null
-                              ? Theme.of(context).disabledColor.withAlpha(150)
-                              : Theme.of(context).disabledColor,
-                        ),
-                  ),
-                ),
-              if (iconData != null)
-                Icon(
-                  iconData,
-                  color: onTap == null
-                      ? Theme.of(context).disabledColor.withAlpha(150)
-                      : Theme.of(context).disabledColor,
-                ),
-            ],
-          ),
-        ),
+        onTap: loading ? null : onTap,
+        onLongPress: loading ? null : onLongPress,
+        child: _buildContent(context),
       ),
     );
   }
+
+  Padding _buildContent(BuildContext context) => Padding(
+        padding: EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (text != null && !loading) _buildText(context),
+            if (iconData != null && !loading) _buildIcon(context),
+            _buildLoader(context),
+          ],
+        ),
+      );
+
+  Opacity _buildLoader(BuildContext context) => Opacity(
+        opacity: loading ? 0.8 : 0,
+        child: SizedBox(
+          width: 18,
+          height: 18,
+          child: FlareActor(
+            "assets/loader.flr",
+            color: Theme.of(context).disabledColor,
+            animation: "Init",
+          ),
+        ),
+      );
+
+  Icon _buildIcon(BuildContext context) => Icon(
+        iconData,
+        color: onTap == null
+            ? Theme.of(context).disabledColor.withAlpha(150)
+            : Theme.of(context).disabledColor,
+      );
+
+  Padding _buildText(BuildContext context) => Padding(
+        padding: EdgeInsets.symmetric(horizontal: 12),
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.button.copyWith(
+                color: onTap == null
+                    ? Theme.of(context).disabledColor.withAlpha(150)
+                    : Theme.of(context).disabledColor,
+              ),
+        ),
+      );
 }
