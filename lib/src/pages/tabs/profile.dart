7a5aa3e9020b91dcf8fd630c6362c63b9a1d83e2
Semyon Butenko <laim0n@yandex.ru>
2019-12-28 01:24:33 +0300

    Update Profile

--- lib/src/pages/tabs/profile.dart
+++ lib/src/pages/tabs/profile.dart
@@ -1,5 +1,12 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:picbox/src/blocs/theme.dart';
+import 'package:picbox/src/blocs/theme/night.dart';
 import 'package:picbox/src/common/bodies/tab.dart';
+import 'package:picbox/src/common/components/list/button.dart';
+import 'package:picbox/src/common/components/list/switch.dart';
 
 class ProfileTab extends StatelessWidget {
   @override
@@ -8,89 +15,53 @@ class ProfileTab extends StatelessWidget {
       child: Column(
         children: <Widget>[
           _buildHead(context),
-          Divider(height: 50),
-          _buildTabsBar(context),
+          SizedBox(height: 20),
+          Divider(),
           _buildBody(context),
         ],
       ),
     );
   }
 
-  Widget _buildTabsBar(context) {
-    return Container(
-      height: 30,
-      margin: EdgeInsets.only(bottom: 10),
-      child: ListView(
-        physics: BouncingScrollPhysics(),
-        scrollDirection: Axis.horizontal,
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
-        children: <Widget>[
-          for (var i = 0; i < 6; i++)
-            Container(
-              width: 90,
-              margin: EdgeInsets.only(right: 15),
-              decoration: BoxDecoration(
-                color: Theme.of(context).disabledColor,
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-              ),
-            ),
-        ],
-      ),
-    );
-  }
-
-  GridView _buildBody(BuildContext context) {
-    return GridView.count(
-      padding: EdgeInsets.all(15),
-      crossAxisSpacing: 12,
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      mainAxisSpacing: 12,
-      crossAxisCount:
-          MediaQuery.of(context).orientation == Orientation.landscape ? 3 : 2,
+  _buildBody(BuildContext context) {
+    return Column(
       children: <Widget>[
-        for (var i = 0; i < 13; i++)
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).disabledColor,
-              borderRadius: BorderRadius.all(Radius.circular(15)),
-            ),
-          ),
+        ListButton(AppLocalizations.of(context).tr('profile.edit_profile'),
+            iconData: MdiIcons.accountEdit),
       ],
     );
   }
 
-  Padding _buildHead(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: 10),
-      child: Column(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).disabledColor,
-              borderRadius: BorderRadius.all(Radius.circular(50)),
-            ),
-            height: 70,
-            width: 70,
-            margin: EdgeInsets.symmetric(vertical: 15),
+    );
+  }
+
+  _buildHead(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 5),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).disabledColor,
+            borderRadius: BorderRadius.all(Radius.circular(50)),
           ),
-          Column(
-            children: <Widget>[
-              Text("Oleg Tinkoff", style: Theme.of(context).textTheme.title),
-              Padding(
-                padding: EdgeInsets.all(4),
-                child: Text(
-                  "@tinkoffoleg",
-                  style: Theme.of(context)
-                      .textTheme
-                      .subhead
-                      .copyWith(color: Theme.of(context).hintColor),
-                ),
-              )
-            ],
-          ),
-        ],
-      ),
+          height: 70,
+          width: 70,
+          margin: EdgeInsets.symmetric(vertical: 15),
+        ),
+        Column(
+          children: <Widget>[
+            Text("Oleg Tinkoff", style: Theme.of(context).textTheme.title),
+            SizedBox(height: 4),
+            Text(
+              "@tinkoffoleg",
+              style: Theme.of(context)
+                  .textTheme
+                  .subhead
+                  .copyWith(color: Theme.of(context).hintColor),
+            )
+          ],
+        ),
+      ],
     );
   }
 }
