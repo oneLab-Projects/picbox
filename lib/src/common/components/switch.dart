38a60a0e9b83aaf78d29c981864b0d3413149afe
Semyon Butenko <laim0n@yandex.ru>
2019-12-28 00:54:59 +0300

    Add custom Switch

---
+++ lib/src/common/components/switch.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Switch extends StatefulWidget {
+  Switch(this.value);
+  bool value;
+
+  @override
+  _SwitchState createState() => _SwitchState();
+}
+
+class _SwitchState extends State<Switch> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 100),
+      width: 30,
+      height: 12,
+      decoration: BoxDecoration(
+          border: Border.all(color: Theme.of(context).primaryColor, width: 2),
+          color: (widget.value
+              ? Theme.of(context).primaryColor
+              : Colors.transparent),
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+    );
+  }
+}
