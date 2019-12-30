4656838159a00cb78aecc25f0894d4c61217b90f
Semyon Butenko <laim0n@yandex.ru>
2019-12-30 23:46:03 +0300

    New tabs system

---
+++ lib/src/common/components/navigator.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'navigation_bar.dart';
+
+class BottomBarTab {
+  final WidgetBuilder routePageBuilder;
+  final WidgetBuilder initPageBuilder;
+  final IconData iconData;
+  final IconData selectedIconData;
+  final bool badge;
+  final GlobalKey<NavigatorState> _navigatorKey;
+
+  BottomBarTab({
+    @required this.initPageBuilder,
+    @required this.iconData,
+    this.selectedIconData,
+    this.badge = false,
+    this.routePageBuilder,
+    GlobalKey<NavigatorState> navigatorKey,
+  }) : _navigatorKey = navigatorKey ?? GlobalKey<NavigatorState>();
+}
+
+class MultiNavigatorBottomBar extends StatefulWidget {
+  final int initTabIndex;
+  final List<BottomBarTab> tabs;
+  final PageRoute pageRoute;
+  final ValueChanged<int> onTap;
+  final Widget Function(Widget) pageWidgetDecorator;
+  final ValueGetter shouldHandlePop;
+
+  final Color backgroundColor;
+  final Color color;
+  final Color selectedColor;
+
+  MultiNavigatorBottomBar({
+    @required this.initTabIndex,
+    @required this.tabs,
+    this.onTap,
+    this.backgroundColor,
+    this.color,
+    this.selectedColor,
+    this.pageRoute,
+    this.pageWidgetDecorator,
+    this.shouldHandlePop = _defaultShouldHandlePop,
+  });
+
+  static bool _defaultShouldHandlePop() => true;
+
+  @override
+  State<StatefulWidget> createState() =>
+      _MultiNavigatorBottomBarState(initTabIndex);
+}
+
+class _MultiNavigatorBottomBarState extends State<MultiNavigatorBottomBar> {
+  int currentIndex;
+
+  _MultiNavigatorBottomBarState(this.currentIndex);
+
+  @override
+  Widget build(BuildContext context) => WillPopScope(
+        onWillPop: () async {
+          return widget.shouldHandlePop()
+              ? !await widget.tabs[currentIndex]._navigatorKey.currentState
+                  .maybePop()
+              : false;
+        },
+        child: Scaffold(
+          body: _buildBottomBar(
+            widget.pageWidgetDecorator == null
+                ? _buildPageBody()
+                : widget.pageWidgetDecorator(_buildPageBody()),
+          ),
+        ),
+      );
+
+  Widget _buildPageBody() => Stack(
+        children:
+            widget.tabs.map((tab) => _buildOffstageNavigator(tab)).toList(),
+      );
+
+  Widget _buildOffstageNavigator(BottomBarTab tab) => Offstage(
+        offstage: widget.tabs.indexOf(tab) != currentIndex,
+        child: TabPageNavigator(
+          navigatorKey: tab._navigatorKey,
+          initPageBuilder: tab.initPageBuilder,
+          pageRoute: widget.pageRoute,
+        ),
+      );
+
+  Widget _buildBottomBar(Widget body) => NavigationBar(
+        body: body,
+        onTabSelected: (index) {
+          if (widget.onTap != null) widget.onTap(index);
+          setState(() => currentIndex = index);
+        },
+        backgroundColor: widget.backgroundColor,
+        color: widget.color,
+        selectedColor: widget.selectedColor,
+        items: widget.tabs
+            .map((tab) => NavigationBarItem(
+                  iconData: tab.iconData,
+                  selectedIconData: tab.selectedIconData,
+                  badge: tab.badge,
+                ))
+            .toList(),
+      );
+}
+
+class TabPageNavigator extends StatelessWidget {
+  TabPageNavigator(
+      {@required this.navigatorKey,
+      @required this.initPageBuilder,
+      this.pageRoute});
+
+  final GlobalKey<NavigatorState> navigatorKey;
+  final WidgetBuilder initPageBuilder;
+  final PageRoute pageRoute;
+
+  @override
+  Widget build(BuildContext context) => Navigator(
+        key: navigatorKey,
+        observers: [HeroController()],
+        onGenerateRoute: (routeSettings) =>
+            pageRoute ??
+            MaterialPageRoute(
+              settings: RouteSettings(isInitialRoute: true),
+              builder: (context) =>
+                  _defaultPageRouteBuilder(routeSettings.name)(context),
+            ),
+      );
+
+  WidgetBuilder _defaultPageRouteBuilder(String routName, {String heroTag}) =>
+      (context) => initPageBuilder(context);
+}
--- lib/src/pages/root.dart
+++ lib/src/pages/root.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:picbox/src/common/components/navigation_bar.dart';
+import 'package:picbox/src/common/components/navigator.dart';
 import 'package:picbox/src/common/design/colors.dart';
 import 'package:picbox/src/pages/tabs.dart';
 
@@ -9,62 +9,36 @@ class RootPage extends StatefulWidget {
   _RootPageState createState() => _RootPageState();
 }
 
-class _RootPageState extends State<RootPage> with TickerProviderStateMixin {
-  TabController _tabController;
-  final List<Widget> _tabs = [
-    HomeTab(),
-    SearchTab(),
-    NotificationsTab(),
-    ProfileTab(),
+class _RootPageState extends State<RootPage> {
+  final List<BottomBarTab> _tabs = [
+    BottomBarTab(
+      initPageBuilder: (context) => HomeTab(),
+      iconData: MdiIcons.compassOutline,
+      selectedIconData: MdiIcons.compass,
+    ),
+    BottomBarTab(
+      initPageBuilder: (context) => SearchTab(),
+      iconData: Icons.search,
+    ),
+    BottomBarTab(
+      initPageBuilder: (context) => NotificationsTab(),
+      iconData: Icons.notifications_none,
+      selectedIconData: Icons.notifications,
+      badge: true,
+    ),
+    BottomBarTab(
+      initPageBuilder: (context) => ProfileTab(),
+      iconData: MdiIcons.accountCircleOutline,
+      selectedIconData: MdiIcons.accountCircle,
+    ),
   ];
 
-  int _currentIndex = 0;
-
   @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    _tabController = TabController(length: _tabs.length, vsync: this);
-    super.initState();
-  }
-
-  void _selectedTab(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: NavigationBar(
-        body: IndexedStack(
-          index: _currentIndex,
-          children: _tabs,
-        ),
-        onTabSelected: _selectedTab,
+  Widget build(BuildContext context) => MultiNavigatorBottomBar(
+        initTabIndex: 0,
+        tabs: _tabs,
         backgroundColor: ColorPalette.bottomNavigation,
         color: Colors.white54,
         selectedColor: Colors.white,
-        items: [
-          NavigationBarItem(
-              iconData: MdiIcons.compassOutline,
-              selectedIconData: MdiIcons.compass),
-          NavigationBarItem(iconData: Icons.search),
-          NavigationBarItem(
-            iconData: Icons.notifications_none,
-            selectedIconData: Icons.notifications,
-            badge: true,
-          ),
-          NavigationBarItem(
-              iconData: MdiIcons.accountCircleOutline,
-              selectedIconData: MdiIcons.accountCircle),
-        ],
-      ),
-    );
-  }
+      );
 }
