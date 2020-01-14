cf7a635e738d6036bc5dc27e7df4a9ab2baad120
Semyon Butenko <laim0n@yandex.ru>
2020-01-14 18:17:38 +0300

    Update SearchPage

--- lib/src/pages/tabs/search.dart
+++ lib/src/pages/tabs/search.dart
@@ -1,39 +1,63 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:picbox/src/common/decorations/input.dart';
 import 'package:picbox/src/common/widgets.dart';
 
 /// Вкладка `Поиск`
 class SearchTab extends StatelessWidget {
+  static const double _cardSize = 150;
+
   @override
   Widget build(BuildContext context) {
     return UScaffold(
       showBackButton: false,
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          children: <Widget>[
-            _buildSearchBar(context),
-            Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).cardColor,
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-              ),
-              height: 185,
-              margin: const EdgeInsets.only(top: 12),
-            ),
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          _buildSearchBar(context),
+          UListContent(
+            AppLocalizations.of(context).tr('search.popular_topics'),
+            iconData: MdiIcons.starFourPoints,
+            variant: true,
+            child: _buildPopularContent(context),
+          ),
+        ],
       ),
     );
   }
 
   Widget _buildSearchBar(context) {
-    return TextField(
-      autocorrect: false,
-      decoration: RoundInputDesign(
-        context,
-        hintText: AppLocalizations.of(context).tr('search.title'),
+    return Padding(
+      padding: const EdgeInsets.all(15).copyWith(bottom: 10),
+      child: TextField(
+        autocorrect: false,
+        decoration: RoundInputDesign(
+          context,
+          hintText: AppLocalizations.of(context).tr('search.title'),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPopularContent(context) {
+    return Container(
+      height: _cardSize,
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      child: ListView(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        children: <Widget>[
+          for (var i = 0; i < 5; i++)
+            Container(
+              width: _cardSize,
+              margin: EdgeInsets.only(right: 10),
+              decoration: BoxDecoration(
+                color: Theme.of(context).highlightColor,
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+            ),
+        ],
       ),
     );
   }
