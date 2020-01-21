89d612498309542b81f8fae8b04f2864f147f591
Semyon Butenko <laim0n@yandex.ru>
2020-01-21 11:23:43 +0300

    Update content card

--- lib/src/common/components/content/card.dart
+++ lib/src/common/components/content/card.dart
@@ -1,71 +1,114 @@
-import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ContentCard extends StatelessWidget {
-  ContentCard(this.title, {this.variant = false});
+  ContentCard(
+    this.title, {
+    this.description,
+    this.urlImage,
+    this.color,
+    this.textColor,
+    this.height,
+    this.width,
+  });
+
+  @required
   final String title;
-  final bool variant;
+  final String description;
+  final Color color;
+  final Color textColor;
+  final String urlImage;
+  final double height;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
-    var color = ColorG.getColor().withOpacity(0.3);
-    return SizedBox(
-      width: !variant ? double.infinity : 150,
-      height: !variant ? double.infinity : 150,
-      child: Material(
-        borderRadius: const BorderRadius.all(Radius.circular(15)),
-        clipBehavior: Clip.antiAliasWithSaveLayer,
-        shadowColor: color,
-        elevation: 3,
+    return _buildBody(
+      context,
+      child: InkWell(
+        onTap: () {},
         child: Stack(
           children: <Widget>[
-            Image.network('https://loremflickr.com/150/150?random=' +
-                Random().nextInt(1000).toString()),
+            if (urlImage != null) _buildBackgroundImage(),
             BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
               child: Container(
-                decoration: BoxDecoration(color: color),
+                color: (color ?? Colors.black).withOpacity(0.3),
               ),
             ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    title,
-                    style: Theme.of(context)
-                        .textTheme
-                        .title
-                        .copyWith(fontSize: 20, fontWeight: FontWeight.w500),
-                  ),
-                  SizedBox(height: 4),
-                  Opacity(
-                    opacity: 0.7,
-                    child: Text(
-                      Random().nextInt(1000).toString(),
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context)
-                          .textTheme
-                          .title
-                          .copyWith(fontSize: 15, fontWeight: FontWeight.w500),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            _buildContent(context),
           ],
         ),
       ),
     );
   }
-}
 
-class ColorG {
-  static Random random = new Random();
-  static Color getColor() {
-    return Color.fromARGB(255, 100 + random.nextInt(155),
-        100 + random.nextInt(55), 100 + random.nextInt(155));
+  Widget _buildContent(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _buildCaption(context),
+          if (description != null) _buildDescription(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCaption(BuildContext context) {
+    return Text(
+      title,
+      style: Theme.of(context).textTheme.title.copyWith(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: textColor ?? Theme.of(context).textTheme.title.color,
+          ),
+    );
+  }
+
+  Widget _buildDescription(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 4),
+      child: Opacity(
+        opacity: 0.7,
+        child: Text(
+          description,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.title.copyWith(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+                color: textColor ?? Theme.of(context).textTheme.title.color,
+              ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBackgroundImage() {
+    return SizedBox.expand(
+      child: FadeInImage.memoryNetwork(
+        fadeInDuration: Duration(milliseconds: 150),
+        placeholder: kTransparentImage,
+        image: urlImage,
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context, {Widget child}) {
+    return SizedBox(
+      width: width ?? double.infinity,
+      height: height ?? double.infinity,
+      child: Material(
+        color: (color ?? Theme.of(context).cardColor),
+        borderRadius: const BorderRadius.all(Radius.circular(15)),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        shadowColor: (color ?? Colors.black).withOpacity(0.3),
+        elevation: 3,
+        child: child,
+      ),
+    );
   }
 }
