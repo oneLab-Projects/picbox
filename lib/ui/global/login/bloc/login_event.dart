4505ac53a2a6cd9b477e884a12e85a66fe528821
Semyon Butenko <laim0n@yandex.ru>
2020-02-12 17:29:52 +0300

    Refactor

--- lib/ui/global/login/bloc/login_event.dart
+++ lib/ui/global/login/bloc/login_event.dart
@@ -1,6 +1,5 @@
 import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
-import 'package:picbox/domain/entity/token.dart';
 
 abstract class LoginEvent extends Equatable {
   const LoginEvent();
