785cb23f059a9eeb7997104b3305deaea101af75
Semyon Butenko <laim0n@yandex.ru>
2020-02-12 15:23:14 +0300

    Update RandomColor

--- lib/util/random_color.dart
+++ lib/util/random_color.dart
@@ -1,13 +1,25 @@
 import 'dart:math';
 import 'dart:ui';
 
-/// Класс, предназначенный для генерации цвета.
-class RandomColor {
+/// Вспомогательный касс, предназначенный для работы с цветом.
+class ColorTool {
   static Random random = new Random();
 
   /// Возвращает случайный, яркий цвет.
   static Color getBrightColor() {
     return Color.fromARGB(255, 100 + random.nextInt(155),
         100 + random.nextInt(55), 100 + random.nextInt(155));
+  }
+
+  /// Возвращает случайный, тёмный цвет.
+  static Color getDarkColor() {
+    return Color.fromARGB(255, 0 + random.nextInt(55), 0 + random.nextInt(55),
+        0 + random.nextInt(55));
+  }
+
+  /// Возвращает тёмный вариант указанного цвета.
+  static Color toDarkColor(Color color) {
+    return Color.fromARGB(
+        color.alpha, color.red - 100, color.green - 100, color.blue - 100);
   }
 }
