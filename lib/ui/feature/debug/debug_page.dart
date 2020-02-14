6fdec6f74cf485018c1a9ef4a3538c734b0d1dbd
Semyon Butenko <laim0n@yandex.ru>
2020-02-14 17:47:36 +0300

    Testing transparent system navbar

--- lib/ui/feature/debug/debug_page.dart
+++ lib/ui/feature/debug/debug_page.dart
@@ -1,9 +1,17 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:picbox/ui/feature/splash/code_input_sheet.dart';
 import 'package:picbox/ui/widget/pansy.dart';
 
 /// Страница `Для разработчиков`.
-class DebugPage extends StatelessWidget {
+class DebugPage extends StatefulWidget {
+  @override
+  _DebugPageState createState() => _DebugPageState();
+}
+
+class _DebugPageState extends State<DebugPage> {
+  bool _value = false;
+
   @override
   Widget build(BuildContext context) {
     return UScaffold(
@@ -14,6 +22,30 @@ class DebugPage extends StatelessWidget {
             "Open splash",
             onPressed: () =>
                 showScaffoldSheet(context, child: CodeInputSheet()),
+          ),
+          UListSwitch(
+            "Test transparent system navbar",
+            value: _value,
+            onChanged: (value) {
+              setState(() => _value = value);
+              if (value) {
+                SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
+                SystemChrome.setSystemUIOverlayStyle(
+                  SystemUiOverlayStyle(
+                    systemNavigationBarColor: Colors.transparent,
+                  ),
+                );
+              } else {
+                SystemChrome.setEnabledSystemUIOverlays(
+                    [SystemUiOverlay.top, SystemUiOverlay.bottom]);
+                SystemChrome.setSystemUIOverlayStyle(
+                  SystemUiOverlayStyle(
+                    systemNavigationBarColor:
+                        Theme.of(context).scaffoldBackgroundColor,
+                  ),
+                );
+              }
+            },
           ),
         ],
       ),
