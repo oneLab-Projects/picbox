42b29952307f98024a5bf7ae4f87541ed6988ff8
Semyon Butenko <laim0n@yandex.ru>
2020-03-17 17:46:47 +0300

    Returned and fixed

--- lib/ui/feature/settings/settings_page.dart
+++ lib/ui/feature/settings/settings_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:pansy_ui/pansy_ui.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:picbox/ui/global/localizations/bloc/bloc.dart';
+import 'package:provider/provider.dart';
+
+import 'localizations_settings_page.dart';
 
 /// Страница `Настройки`.
 class SettingsPage extends StatelessWidget {
@@ -11,12 +16,12 @@ class SettingsPage extends StatelessWidget {
       title: 'settings.title'.tr(),
       body: Column(
         children: <Widget>[
-          //_buildLanguagePageViewButton(context),
+          _buildLanguagePageViewButton(context),
         ],
       ),
     );
   }
-/*
+
   /// Создаёт кнопку для перехода на страницу `Язык интерфейса`
   Widget _buildLanguagePageViewButton(BuildContext context) {
     var supportedLocales =
@@ -26,9 +31,9 @@ class SettingsPage extends StatelessWidget {
     return UListSelect(
       'settings.localizations.title'.tr(),
       iconData: MdiIcons.earth,
-      value: supportedLocales[localeName],
+      value: localeName,
       onPressed: () => Navigator.push(context,
           UPageRoute(builder: (context) => LocalizationsSettingPage())),
     );
-  }*/
+  }
 }
