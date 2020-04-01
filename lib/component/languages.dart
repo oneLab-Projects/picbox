91e45fb06f836742d5778ba65d2cc4a701dbf9b2
Semyon Butenko <laim0n@yandex.ru>
2020-04-01 10:34:58 +0300

    Refactor and fix
    [publish]
    - Исправления в коде
    - Переписана локализация
    - Переписана система BLoC-ов

---
+++ lib/component/languages.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+abstract class Languages {
+  static Map<Locale, String> all = {
+    Locale('en'): 'English',
+    Locale('ru'): 'Русский',
+    Locale('ar'): 'Отзеркаленный',
+  };
+}
